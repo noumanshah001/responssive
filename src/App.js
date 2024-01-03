15e8578435b1324c90963011046365b3eeeca630
Nouman2313 <snouman729@gmail.com>
2024-01-03 20:24:15 +0500

    react app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <div> 
+        <h1 className="text-2xl font-bold text-gray-500 text-center ml-10 mt-10 ">Offerings</h1>
+      </div>
+
+      <div className='card1'>
+        <div className="circle1 bg-yellow-400 rounded-full w-28 h-28 text-center -mt-16 ml-72 flex justify-center items-center text-5xl font-bold text-gray-50 absolute">01</div>
+        <div class="triangle-right text-1xl font-bold text-gray-500 ml-20 mt-32"></div>
+        <div className="text-1xl font-bold text-slate-50 ml-64 -mt-36">Early Year Programme</div>
+        <div className=" font-normal font-bold  text-gray-400 ml-60 -mt-22">Tailwind includes an expertly <br></br>crafted default color palette <br></br>out-of-the-box that  great.</div>
+        <div class="triangle1"></div>
+      </div>
+
+      <div className='card2'>
+        <div className="circle1 bg-yellow-400 rounded-full w-28 h-28 text-center -mt-16 ml-72 flex justify-center items-center text-5xl font-bold text-gray-50 absolute">02</div>
+        <div class="triangle-right text-1xl font-bold text-gray-500 ml-20 mt-32"></div>
+        <div className="text-1xl font-bold text-slate-50 ml-64 -mt-36">Early Year Programme</div>
+        <div className=" font-normal font-bold  text-gray-400 ml-60 -mt-22">Tailwind includes an expertly <br></br>crafted default color palette <br></br>out-of-the-box that  great.</div>
+        <div class="triangle1"></div>
+      </div>
+
+      <div className='card3'>
+        <div className="circle1 bg-yellow-400 rounded-full w-28 h-28 text-center -mt-16 ml-72 flex justify-center items-center text-5xl font-bold text-gray-50 absolute">03</div>
+        <div class="triangle-right text-1xl font-bold text-gray-500 ml-20 mt-32"></div>
+        <div className="text-1xl font-bold text-slate-50 ml-64 -mt-36">Early Year Programme</div>
+        <div className=" font-normal font-bold  text-gray-400 ml-60 -mt-22">Tailwind includes an expertly <br></br>crafted default color palette <br></br>out-of-the-box that  great.</div>
+        <div class="triangle1"></div>
+      </div>
+    </div>
+  );
+}
+export default App;
